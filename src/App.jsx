6ddc10968a3a5720b60c691313b06955ef6e19e5
Miguel Rivas <mrivaslezama@gmail.com>
2023-09-10 21:31:52 -0300

    actualizando setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import Header from './components/Header'
+import Hero from './components/Hero';
+import Demo from './components/Demo';
+import './App.css';
 
 const App = () => {
   return (
-    <div>App</div>
+    <main>
+      <div className='main'>
+        <div className='gradient'>
+        </div>
+      </div>
+    </main>
     
   )
 }
